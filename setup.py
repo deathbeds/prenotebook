0948370bef7227917c13cd08b2ced0ddf29a414c
Tony Fast <tony.fast@gmail.com>
2020-01-13 14:13:31 -0500

    swap setip

---
+++ setup.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+import setuptools
+
+name = "prenotebook"
+
+__version__ = 0.1.0
+
+setup_args = dict(
+    name=name,
+    version="0.1.0",
+    author="deathbeds",
+    packages=setuptools.find_packages(),
+    install_requires="importnb black isort".split(),
+    author_email="tony.fast@gmail.com",
+    description="Import Jupyter (ne IPython) notebooks into tests and scripts.",
+)
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
