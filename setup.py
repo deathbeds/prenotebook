0459e941751dc3d22939d6e515db7977533aaedc
Tony Fast <tony.fast@gmail.com>
2020-01-30 14:26:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ setup_args = dict(
     version="0.1.0",
     author="deathbeds",
     packages=setuptools.find_packages(),
-    install_requires=Path('requirements.txt').read_text().split(),
+    install_requires=Path("requirements.txt").read_text().split(),
     author_email="tony.fast@gmail.com",
     description="Import Jupyter (ne IPython) notebooks into tests and scripts.",
+    entry_points={"console_scripts": ["prenotebook=prenotebook:main"],},
 )
 
 if __name__ == "__main__":
